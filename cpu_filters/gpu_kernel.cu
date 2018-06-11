c4ebe8c6883967ee47af8f3b1a5bb61db32eef74
Josh Wilson <jwils019@ucr.edu>
2018-06-11 16:54:27 -0700

    back to bender

--- cpu_filters/gpu_kernel.cu
+++ cpu_filters/gpu_kernel.cu
@@ -1,7 +1,6 @@
 #include <stdio.h>
    
 __global__ void gpu_filter(int n, const float *A, const float *B, float* C) {
-	unsigned int i = blockIdx.x*256+threadIdx.x;
-	if (i < n) C[i] = A[i] + B[i];
+
 	return;
 }
--- project/main.cu
+++ project/main.cu
@@ -1,137 +1,58 @@
-/******************************************************************************
- *
- *            (C) Copyright 2010 The Board of Trustees of the
- *                        University of Illinois
- *                         All Rights Reserved
- *
- ******************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "kernel.cu"
 #include "support.h"
-
-int main (int argc, char *argv[])
+void gpu_blur (string filename)
 {
+BMP Background;
+Background.ReadFromFile(filename.c_str());
+int height = Background.TellHeight();
+int width = Background.TellWidth();
+int depth = Background.TellBitDepth();
+BMP Output;
+Output.SetSize( width , height );
+Output.SetBitDepth( 24 );
 
-    Timer timer;
-    cudaError_t cuda_ret;
+cudaError_t cuda_ret;
 
-    // Initialize host variables ----------------------------------------------
+ebmpBYTE *A_h, *B_h;
+ebmpBYTE *A_d, *B_d;
+A_h = *Background.Pixels;
+B_h = *Output.Pixels;
+dim3 dim_grid, dim_block;
 
-    printf("\nSetting up the problem..."); fflush(stdout);
-    startTime(&timer);
+cudaMalloc((void**)&A_d, sizeof(ebmpBYTE)*width*height);
+cudaMalloc((void**)&B_d, sizeof(ebmpBYTE)*width*height);
 
-    float *A_h, *B_h, *C_h;
-    float *A_d, *B_d, *C_d;
-    size_t A_sz, B_sz, C_sz;
-    unsigned matArow, matAcol;
-    unsigned matBrow, matBcol;
-    dim3 dim_grid, dim_block;
+cudaDeviceSynchronize();
 
-    if (argc == 1) {
-        matArow = 1000;
-        matAcol = matBrow = 1000;
-        matBcol = 1000;
-    } else if (argc == 2) {
-        matArow = atoi(argv[1]);
-        matAcol = matBrow = atoi(argv[1]);
-        matBcol = atoi(argv[1]);
-    } else if (argc == 4) {
-        matArow = atoi(argv[1]);
-        matAcol = matBrow = atoi(argv[2]);
-        matBcol = atoi(argv[3]);
-    } else {
-        printf("\n    Invalid input parameters!"
-      "\n    Usage: ./sgemm-tiled                # All matrices are 1000 x 1000"
-      "\n    Usage: ./sgemm-tiled <m>            # All matrices are m x m"
-      "\n    Usage: ./sgemm-tiled <m> <k> <n>    # A: m x k, B: k x n, C: m x n"
-      "\n");
-        exit(0);
-    }
+cudaMemcpy(A_d, A_h, sizeof(ebmpBYTE)*width*height, cudaMemcpyHostToDevice);
 
-    A_sz = matArow*matAcol;
-    B_sz = matBrow*matBcol;
-    C_sz = matArow*matBcol;
+cudaDeviceSynchronize();
 
-    A_h = (float*) malloc( sizeof(float)*A_sz );
-    for (unsigned int i=0; i < A_sz; i++) { A_h[i] = (rand()%100)/100.00; }
+//FIXME KERNEL CALL HERE
 
-    B_h = (float*) malloc( sizeof(float)*B_sz );
-    for (unsigned int i=0; i < B_sz; i++) { B_h[i] = (rand()%100)/100.00; }
+cuda_ret = cudaDeviceSynchronize();
+if(cuda_ret != cudaSuccess) FATAL("Unable to launch kernel");
 
-    C_h = (float*) malloc( sizeof(float)*C_sz );
+cudaMemcpy(B_h, B_d, sizeof(ebmpBYTE)*width*height, cudaMemcpyDeviceToHost);
 
-    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
-    printf("    A: %u x %u\n    B: %u x %u\n    C: %u x %u\n", matArow, matAcol,
-        matBrow, matBcol, matArow, matBcol);
+cudaDeviceSynchronize();
 
-    // Allocate device variables ----------------------------------------------
+string fileout = filename;
+fileout.pop_back();
+fileout.pop_back();
+fileout.pop_back();
+fileout.pop_back();
+string extra = "_cpu_blur.bmp";
+fileout = fileout + extra;
+Output.WriteToFile(fileout.c_str());
 
-    printf("Allocating device variables..."); fflush(stdout);
-    startTime(&timer);
-
-    //INSERT CODE HERE
-    cudaMalloc((void**)&A_d, sizeof(float)*A_sz);
-    cudaMalloc((void**)&B_d, sizeof(float)*B_sz);
-    cudaMalloc((void**)&C_d, sizeof(float)*C_sz);
-
-    cudaDeviceSynchronize();
-    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
-
-    // Copy host variables to device ------------------------------------------
-
-    printf("Copying data from host to device..."); fflush(stdout);
-    startTime(&timer);
-
-    //INSERT CODE HERE
-    cudaMemcpy(A_d, A_h, A_sz*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(B_d, B_h, B_sz*sizeof(float), cudaMemcpyHostToDevice);
-
-    cudaDeviceSynchronize();
-    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
-
-    // Launch kernel using standard sgemm interface ---------------------------
-    printf("Launching kernel..."); fflush(stdout);
-    startTime(&timer);
-    basicSgemm('N', 'N', matArow, matBcol, matBrow, 1.0f, \
-		A_d, matArow, B_d, matBrow, 0.0f, C_d, matBrow);
-
-    cuda_ret = cudaDeviceSynchronize();
-	if(cuda_ret != cudaSuccess) FATAL("Unable to launch kernel");
-    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
-
-    // Copy device variables from host ----------------------------------------
-
-    printf("Copying data from device to host..."); fflush(stdout);
-    startTime(&timer);
-
-    //INSERT CODE HERE
-    cudaMemcpy(C_h, C_d, C_sz*sizeof(float), cudaMemcpyDeviceToHost);
-
-    cudaDeviceSynchronize();
-    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
-
-    // Verify correctness -----------------------------------------------------
-
-    printf("Verifying results..."); fflush(stdout);
-
-    verify(A_h, B_h, C_h, matArow, matAcol, matBcol);
-
-
-    // Free memory ------------------------------------------------------------
-
-    free(A_h);
-    free(B_h);
-    free(C_h);
-
-    //INSERT CODE HERE
-
-    cudaFree(C_d);
-    cudaFree(B_d);
-    cudaFree(A_d);
-
-    return 0;
-
+free(A_h);
+free(B_h);
+cudaFree(B_d);
+cudaFree(A_d);
+return(0);
 }
+
 
