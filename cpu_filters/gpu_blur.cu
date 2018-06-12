ba552291f8473e7c249f8c7f7213efc436dbd7c2
Josh Wilson <jwils019@ucr.edu>
2018-06-11 22:32:25 -0700

    back to bender again

--- cpu_filters/gpu_blur.cu
+++ cpu_filters/gpu_blur.cu
@@ -1,6 +1,7 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "gpu_kernel.cu"
-using namespace std;
+#include "support.h"
 void gpu_blur (string filename)
 {
 BMP Background;
@@ -12,31 +13,57 @@ BMP Output;
 Output.SetSize( width , height );
 Output.SetBitDepth( 24 );
 
-    cudaError_t cuda_ret;
+cudaError_t cuda_ret;
 
-    ebmpBYTE *A_h, *B_h;
-    ebmpBYTE *A_d, *B_d;
-    A_h = *Background.Pixels;
-    B_h = *Output.Pixels;
-    dim3 dim_grid, dim_block;
+int *weigths, *weights_d;
 
-    cudaMalloc((void**)&A_d, sizeof(ebmpBYTE)*width*height);
-    cudaMalloc((void**)&B_d, sizeof(ebmpBYTE)*width*height);
+weights = (int*) malloc( sizeof(int)*18 );
 
-    cudaDeviceSynchronize();
 
-    cudaMemcpy(A_d, A_h, sizeof(ebmpBYTE)*width*height, cudaMemcpyHostToDevice);
+//SET WEIGHTS:
+for(int i = 0; i < 9; i++){
+weights[i] = 1;
+}
+weights[10] = 16;
+weights[11] = 8;
+weights[12] = 16;
+weights[13] = 8;
+weights[14] = 4;
+weights[15] = 8;
+weights[16] = 16;
+weights[17] = 8;
+weights[18] = 16;
 
-    cudaDeviceSynchronize();
+//WEIGHTS SET
 
-    //FIXME KERNEL CALL HERE
+ebmpBYTE *A_h, *B_h;
+ebmpBYTE *A_d, *B_d;
+A_h = *Background.Pixels;
+B_h = *Output.Pixels;
+dim3 dim_grid, dim_block;
 
-    cuda_ret = cudaDeviceSynchronize();
-	if(cuda_ret != cudaSuccess) FATAL("Unable to launch kernel");
+cudaMalloc((void**)&weights_d, sizeof(int)*18 );
+cudaMalloc((void**)&A_d, sizeof(ebmpBYTE)*width*height*4);
+cudaMalloc((void**)&B_d, sizeof(ebmpBYTE)*width*height*4);
 
-    cudaMemcpy(B_h, B_d, sizeof(ebmpBYTE)*width*height, cudaMemcpyDeviceToHost);
+cudaDeviceSynchronize();
 
-    cudaDeviceSynchronize();
+cudaMemcpy(weights_d, weights, sizeof(int)*18, cudaMemcpyHostToDevice);
+cudaMemcpy(A_d, A_h, sizeof(ebmpBYTE)*width*height*4, cudaMemcpyHostToDevice);
+
+cudaDeviceSynchronize();
+
+dim3 DimGrid(1, 1, 1);
+dim3 DimBlock(1024, 1, 1);
+
+gpu_filter<<<DimGrid, DimBlock>>>(A_d, B_d, weights_d, width, height);
+
+cuda_ret = cudaDeviceSynchronize();
+if(cuda_ret != cudaSuccess) FATAL("Unable to launch kernel");
+
+cudaMemcpy(B_h, B_d, sizeof(ebmpBYTE)*width*height*4, cudaMemcpyDeviceToHost);
+
+cudaDeviceSynchronize();
 
 string fileout = filename;
 fileout.pop_back();
@@ -46,15 +73,14 @@ fileout.pop_back();
 string extra = "_cpu_blur.bmp";
 fileout = fileout + extra;
 Output.WriteToFile(fileout.c_str());
+free(weights);
+free(A_h);
+free(B_h);
 
-    free(A_h);
-    free(B_h);   
-    cudaFree(B_d);
-    cudaFree(A_d);   
-    return(0);
+cudaFree(weights_d);
+cudaFree(B_d);
+cudaFree(A_d);
+return(0);
 }
-
-
-
 
 
