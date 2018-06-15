5f52be49a56687fa3501237caa37b4648b8cdc9b
Josh Wilson <jwils019@ucr.edu>
2018-06-14 20:28:32 -0700

    removed cpu b/w because we have it in python for both cpu and gpu

--- c++_files/filters.h
+++ c++_files/filters.h
@@ -4,7 +4,6 @@
 #include "cpu_edges.cpp"
 #include "cpu_emboss.cpp"
 #include "cpu_sobel.cpp"
-#include "cpu_black_and_white.cpp"
 #include "kernel.cu"
 #include "gpu_blur.cu"
 #include "gpu_sharpen.cu"
